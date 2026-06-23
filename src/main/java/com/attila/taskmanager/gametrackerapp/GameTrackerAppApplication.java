4e72cabba624d62eaf63b27e9b2779b2cae6de5e
Viszket Attila <viszket.attila1995@gmail.com>
2026-06-23 17:18:16 +0200

    project init

---
+++ src/main/java/com/attila/taskmanager/gametrackerapp/GameTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.attila.taskmanager.gametrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameTrackerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameTrackerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/attila/taskmanager/gametrackerapp/GameTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attila.taskmanager.gametrackerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameTrackerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
